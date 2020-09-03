e06aa47bb390201b099a5b1e8817d753defca7cf
Rémy Hidra <remy.hidra@gmail.com>
2020-09-03 14:27:33 +0200

    Updates

---
+++ precache-manifest.8cc76ae5b52c4a0ff28a5ab5c044db5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e0b61e97792acf75826049e1e612833",
+    "url": "/now-chat/index.html"
+  },
+  {
+    "revision": "4e8bb0195f6f37dd18c6",
+    "url": "/now-chat/static/css/main.4316c1af.chunk.css"
+  },
+  {
+    "revision": "fa28b5279dbdb08bf699",
+    "url": "/now-chat/static/js/2.6edbc946.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/now-chat/static/js/2.6edbc946.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e8bb0195f6f37dd18c6",
+    "url": "/now-chat/static/js/main.cdb214ab.chunk.js"
+  },
+  {
+    "revision": "a9328226489a85ed4dcc",
+    "url": "/now-chat/static/js/runtime-main.beabb59a.js"
+  }
+]);
